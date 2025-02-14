bc54f1c4c32f120100a2787b62d568f72079be6b
pawitchaya <pawichaya.p@quadel.co.th>
2025-02-14 17:08:09 +0700

    four build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2994862918"
+    serviceWorkerVersion: "2360776930"
   }
 });
